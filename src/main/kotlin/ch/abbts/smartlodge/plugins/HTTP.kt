b74da15593b047d8f89e95c7b223cddc4b71b902
Hannes Morgenthaler <hanneszbinden@outlook.com>
2024-06-01 15:27:35 +0200

    upgrade to gradle 8.8.

--- src/main/kotlin/ch/abbts/smartlodge/plugins/HTTP.kt
+++ src/main/kotlin/ch/abbts/smartlodge/plugins/HTTP.kt
@@ -3,7 +3,6 @@ package ch.abbts.smartlodge.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.httpsredirect.*
 import io.ktor.server.plugins.swagger.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureHTTP() {
