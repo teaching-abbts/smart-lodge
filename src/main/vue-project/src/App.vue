ccce85a9661bc2c8fa8ba0c0390d156555f9ac48
Hannes Morgenthaler <hanneszbinden@outlook.com>
2024-06-06 22:04:30 +0200

    add primitive fotoalbum.

--- src/main/vue-project/src/App.vue
+++ src/main/vue-project/src/App.vue
@@ -1,47 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const fotoalbum = ref()
+
+onMounted(() => {
+  fetch('/fotoalbum')
+    .then((response) => response.json())
+    .then((payload) => (fotoalbum.value = payload))
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="fotoalbum" v-if="fotoalbum">
+    <img v-for="foto in fotoalbum.fotos" :key="foto.url" :src="foto.url" :alt="foto.name" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.fotoalbum {
+  display: flex;
+  flex-direction: row;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+img {
+  width: 400px;
 }
 </style>
