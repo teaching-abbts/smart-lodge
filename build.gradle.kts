7d1053429fa590068b2f8886bedda2920c8553b9
Hannes Morgenthaler <hanneszbinden@outlook.com>
2024-06-28 11:46:29 +0200

    use ktor_version for dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,15 +25,15 @@ repositories {
 }
 
 dependencies {
-  implementation("io.ktor:ktor-server-core-jvm")
-  implementation("io.ktor:ktor-server-freemarker-jvm")
-  implementation("io.ktor:ktor-server-content-negotiation-jvm")
-  implementation("io.ktor:ktor-server-host-common-jvm")
-  implementation("io.ktor:ktor-server-netty-jvm")
+  implementation("io.ktor:ktor-server-core:$ktor_version")
+  implementation("io.ktor:ktor-server-freemarker:$ktor_version")
+  implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+  implementation("io.ktor:ktor-server-host-common:$ktor_version")
+  implementation("io.ktor:ktor-server-netty:$ktor_version")
   implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
   implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktor_version")
   implementation("ch.qos.logback:logback-classic:$logback_version")
-  testImplementation("io.ktor:ktor-server-tests-jvm")
+  testImplementation("io.ktor:ktor-server-tests:$ktor_version")
   testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
